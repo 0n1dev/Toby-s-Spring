a610eb630f633ab23ddcc0513474d6454b6a6d07
0n1dev <all_mind@naver.com>
2021-05-17 12:30:30 +0900

    2일차 토비스프링 챕터 1-3

---
+++ chapter1-3/src/main/java/com/example/demo/user/dao/MySQLDao.java
@@ -0,0 +1,51 @@
+package com.example.demo.user.dao;
+
+import com.example.demo.connection.ConnectionMaker;
+import com.example.demo.user.domain.User;
+
+import java.sql.*;
+
+public class MySQLDao {
+
+    private ConnectionMaker connectionMaker;
+
+    public MySQLDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Connection c = connectionMaker.makeConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        Connection c = connectionMaker.makeConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
---
+++ chapter1-3/src/test/java/com/example/demo/user/dao/MySQLDaoTest.java
@@ -0,0 +1,33 @@
+package com.example.demo.user.dao;
+
+import com.example.demo.connection.FConnection;
+import com.example.demo.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySQLDaoTest {
+
+    @Test
+    void testConnection() throws SQLException, ClassNotFoundException {
+        final MySQLDao userDao = new MySQLDao(new FConnection());
+
+        User user = new User();
+
+        user.setId("spring3");
+        user.setName("springtest3");
+        user.setPassword("password");
+
+        userDao.add(user);
+
+        User getUser = userDao.get(user.getId());
+
+        assertThat(getUser.getId()).isEqualTo(user.getId());
+        assertThat(getUser.getName()).isEqualTo(user.getName());
+        assertThat(getUser.getPassword()).isEqualTo(user.getPassword());
+    }
+}
